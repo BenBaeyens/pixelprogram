31ff09b1de26fc983fd221e0a5a8948efdca3a54
Kjip <kjipgamer@gmail.com>
2019-07-06 17:29:18 +0200

    Added 3 base colors to colorpicker

--- PixelProgram/Assets/ColorPicker.cs
+++ PixelProgram/Assets/ColorPicker.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 public class ColorPicker : MonoBehaviour
 {
 
+    ColorChanger colorChanger;
 
+    Material[] colors;
 
     #region Methods
 
     void Start()
     {
-        
+        colorChanger = GameObject.Find("Selector").GetComponent<ColorChanger>();
+    }
+
+    public void PickColor(string color){
+        switch(color){
+            case "White":
+                colorChanger.effectedMat = colors[0];
+                break;
+            case "Grey":
+                colorChanger.effectedMat = colors[1];
+                break;
+            case "Black":
+                colorChanger.effectedMat = colors[2];
+                break;
+            default:
+                Debug.LogError("Error: Incorrect color argument");
+                break;
+
+        }
+
     }
 
     #endregion
---
+++ PixelProgram/Assets/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+
+
+
+    #region Methods
+
+    void Start()
+    {
+        
+    }
+
+    #endregion
+}
