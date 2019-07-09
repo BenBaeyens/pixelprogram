9edfc49ca9742b0113a621f5883d7d16f540875e
Kjip <kjipgamer@gmail.com>
2019-07-09 23:51:15 +0200

    Added undo function

--- PixelProgram/Assets/ColorChanger.cs
+++ PixelProgram/Assets/ColorChanger.cs
@@ -10,14 +10,28 @@ public class ColorChanger : MonoBehaviour
     [SerializeField] SelectorController selectorController;
     ColorUndo colorUndo;
 
+    public List<GameObject> pixelHistory;
+
     #region Methods
 
+
+    private void Start() {
+        pixelHistory = new List<GameObject>();
+    }
     private void Update() {
         if(Input.GetKey(KeyCode.Space)){
             selectorController.floorObject.GetComponent<Renderer>().material = effectedMat; 
             colorUndo = selectorController.floorObject.GetComponent<ColorUndo>();
             colorUndo.AddColor();
+            pixelHistory.Add(selectorController.floorObject);
         }   
+
+        if(Input.GetKeyDown(KeyCode.U)){
+            if(pixelHistory.Count >= 2){
+                pixelHistory[pixelHistory.Count - 1].GetComponent<ColorUndo>().UndoColor();
+                pixelHistory.RemoveAt(pixelHistory.Count - 1);
+            }
+        }
     }
 
     #endregion
