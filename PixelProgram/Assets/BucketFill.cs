73700f6b26070e881605a34cb86a4ebb4f8d0d8e
Kjip <kjipgamer@gmail.com>
2019-07-14 21:47:19 +0200

    Added bucket fill

---
+++ PixelProgram/Assets/BucketFill.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BucketFill : MonoBehaviour
+{
+
+    public List<GameObject> pixels;
+    [SerializeField] ColorChanger colorChanger;
+    [SerializeField] GameObject pixelParent;
+
+
+    #region Methods
+
+    public void PixelArray()
+    {
+        pixels = new List<GameObject>();
+        foreach (Transform child in pixelParent.transform)
+        {
+            pixels.Add(child.gameObject);
+        }
+    }
+
+    public void FillBucket(){
+        for (int i = 0; i < pixels.Count; i++)
+        {
+            pixels[i].GetComponent<Renderer>().material = colorChanger.effectedMat;
+            pixels[i].GetComponent<ColorUndo>().AddColor();
+        }
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.B))
+            FillBucket();
+    }
+    
+
+    #endregion
+}
