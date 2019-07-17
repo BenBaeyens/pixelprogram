cdbf1c0ad993477f5e266c38e0d28a6c512fbf2e
Kjip <kjipgamer@gmail.com>
2019-07-17 19:20:12 +0200

    Added functionality to default size buttons

--- PixelProgram/Assets/Settings.cs
+++ PixelProgram/Assets/Settings.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Settings : MonoBehaviour
 {
 
-
+    [SerializeField] TMP_InputField xCoord;
+    [SerializeField] TMP_InputField yCoord;
 
     #region Methods
 
-    void Start()
-    {
-        
+    public void ChangeDefaultSize(){
+        PlayerPrefs.SetInt("DefaultSizeX", int.Parse(xCoord.text));
+        PlayerPrefs.SetInt("DefaultSizeY", int.Parse(yCoord.text));
     }
 
     #endregion
